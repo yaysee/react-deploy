aeb820fe8b24d9155051f01387cc86c0ead2dcab
yaysee <jamesiesi1000@gmail.com>
2021-09-28 19:43:08 +0100

    added nav

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url:'#',
+        cName: 'nav-links',
+
+    },
+    {
+        title: 'Pictures',
+        url:'#',
+        cName: 'nav-links',
+
+    },
+    {
+        title: 'Share',
+        url:'#',
+        cName: 'nav-links',
+
+    },
+    {
+        title: 'Contact Us',
+        url:'#',
+        cName: 'nav-links',
+
+    },
+    {
+        title: 'Home',
+        url:'#',
+        cName: 'nav-links-mobile',
+
+    },
+]

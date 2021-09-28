06731b10b5ad6d6e79281c3c39bfaf085025040c
yaysee <jamesiesi1000@gmail.com>
2021-09-28 16:08:56 +0100

    here

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome yall
+          WELCOME ME
         </p>
         <a
           className="App-link"
